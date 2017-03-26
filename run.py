531bd3db54e0af976154b834e66237037607dbbd
prachi285 <prachi.28.tiwari@gmail.com>
2017-03-27 02:31:36 +0530

    add data

--- run.py
+++ run.py
@@ -8,7 +8,18 @@ queries = ['how to handle a 1.5 year old when hitting',
  'how do male penguins survive without eating for four months',
  'how do i remove candle wax from a polar fleece',
  'how do i find an out of print book',
- 'yeh query hai']
+ 'yeh query hai',
+ 'Aap Kaise Hain',
+'I am Fine Main theek Hoon',
+'We’ll meet again',
+ 'See you latter Phir Milenge',
+'What’s your name? Aap ka naam kya hai',
+'My Name is <name> Mera Naam <name> Hai',
+'I am Going Main Ja Raha Hoon, Main Ja Rahi Hoon',
+'Where are you going? Aap kahan ja rahe ho?',
+'Where are we going? Hum kahan ja rahe hain?',
+'How is your family? Aapka parivaar kaisa hai?''
+'Nice to meet you Aapse mil kar khushi hu]''
 
 # train the model
 
